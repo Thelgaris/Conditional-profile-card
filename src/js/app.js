0780dbac859e4ddbd6523fcc2cadcb6782638e2a
Thelgaris <alex.gabarron84@gmail.com>
2022-03-19 12:12:48 +0000

    testtwo

--- src/js/app.js
+++ src/js/app.js
@@ -28,8 +28,13 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-  let nombre = variables.name;
-  if (variables.name == null) nombre = "Type your name"; // reset the website body with the new html output
+  if (variables.name == null) variables.name = "Name";
+  if (variables.lastname == null) variables.lastname = "LastName";
+  if (variables.role == null) variables.role = "Role";
+  if (variables.country == null) variables.country = "Country";
+  if (variables.city == null) variables.city = "City";
+
+  // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
          
@@ -38,10 +43,10 @@ function render(variables = {}) {
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
           <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
